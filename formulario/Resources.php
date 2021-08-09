c0681a2eacbf8e403f9cd5f3607ccbd38ad0fa21
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-09 18:25:37 +0200

    Added save function

--- formulario/Resources.php
+++ formulario/Resources.php
@@ -6,8 +6,7 @@ class Resources{
  * @param array $array_data array to be checked. Insert either error of bien if values are right or wrong
  * @return array $arr array with the verified elements divided in error and good
  */
-    function validacion(array $array_data): array{
-        //$arr = [];
+    function validacion(array $array_data): array{        
         $arr = ['error' => [], 'good' => []];
         foreach ($array_data as $key => $value) {
             $cadenaVacia = trim($value);
@@ -20,6 +19,15 @@ class Resources{
         }
         return $arr;
     }
-    
+
+    function save(array $array_data){
+    $array_save = $this->validacion($array_data);    
+		if(count($array_save['error']) === 0){
+            return true;
+        }
+        else{
+            return $array_save;
+        }
+    }  
 }
 ?>
