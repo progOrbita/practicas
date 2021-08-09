f750690d0f34c53b090975487893c117c4ac74f2
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-09 12:17:24 +0200

    extraido la validacion a una clase

--- formulario/validacion.php
+++ formulario/validacion.php
@@ -9,17 +9,12 @@
 
 
 */
-	
+	include 'Resources.php';
 if(isset($_POST['texto'])){
-	$arr = [];
+	$validate = new Resources();
 	$array_datos = ["texto" => $_POST['texto'],"numerico" => $_POST['numerico'],"fecha" => $_POST['fecha']];
-	foreach ($array_datos as $key => $value) {
-		$cadenaVacia = trim($value);
-		if(empty($cadenaVacia)){
-			array_push($arr, $key);
-		}
-	}
-	echo json_encode($arr);
+	$datos = $validate->validacion($array_datos);
+	echo json_encode($datos);
 
 }
 
