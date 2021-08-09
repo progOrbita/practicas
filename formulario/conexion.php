6e559e7b7a6c9696aa6242c692c81fb4bfbadc15
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-09 17:37:43 +0200

    add check and do the query to the database

--- formulario/conexion.php
+++ formulario/conexion.php
@@ -3,12 +3,19 @@
 			require_once '../../config/config.inc.php';
 			require_once '../../init.php';
 		}
+		include "Resources.php";
+		
 		$database = Db::getInstance();
+		$validados = new Resources();
 		if(isset($_POST['texto'])){
-			$name = $_POST['texto'];
-			$number = $_POST['numerico'];
-			$date = $_POST['fecha'];
-			$query = $database->execute('INSERT INTO test.tableto(name,number,fecha,fecha_creacion) VALUES ("'.$name.'",'.$number.',"'.$date.'",NOW())');		
-		}		
+				$name = $_POST['texto'];
+				$number = $_POST['numerico'];
+				$date = $_POST['fecha'];
+				$array_datos = ["texto" => $_POST['texto'],"numerico" => $_POST['numerico'],"fecha" => $_POST['fecha']];
+			$cadena = $validados->validacion($array_datos);		
+			if(sizeof($cadena['error']) === 0){
+				$query = $database->execute('INSERT INTO test.tableto(name,number,fecha,fecha_creacion) VALUES ("'.$name.'",'.$number.',"'.$date.'",NOW())');
+			}			
+		}
 		
 	?>
