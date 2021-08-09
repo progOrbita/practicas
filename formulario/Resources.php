53f7797334d0be332983c425cc5244ed4aef55c1
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-09 16:36:29 +0200

    changed arr to good and bad

--- formulario/Resources.php
+++ formulario/Resources.php
@@ -7,14 +7,15 @@ class Resources{
  * @return array $arr el array con los datos ya verificados
  */
     function validacion(array $array_data): array{
-        $arr = [];
+        //$arr = [];
+        $arr = ['error' => [], 'bien' => []];
         foreach ($array_data as $key => $value) {
             $cadenaVacia = trim($value);
             if(empty($cadenaVacia)){
-                array_push($arr, $key);
+                array_push($arr['error'], $key);
             }
             else{
-                array_push($arr,$value);
+                array_push($arr['bien'],$value);
             }
         }
         return $arr;
