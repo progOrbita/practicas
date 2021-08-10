420e3914fe61394e9008caeeae10f3586ad0d499
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-10 11:03:15 +0200

    reworked delete to return an array (empty) or true/false with the query

--- formulario/Resources.php
+++ formulario/Resources.php
@@ -1,6 +1,7 @@
 <?php
 
 class Resources{
+    protected $table = _DB_PREFIX_.'formulario';
 /**
  * validate an array of elements, used for the form
  * @param array $array_data array to be checked. Insert either error of bien if values are right or wrong
@@ -27,7 +28,7 @@ class Resources{
     function save(array $array_data){
     $array_save = $this->validacion($array_data);    
 		if(count($array_save['error']) === 0){           
-            $query = Db::getInstance()->execute('INSERT INTO \'_DB_PREFIX_\'.formulario(name,number,fecha,fecha_creacion) VALUES ("'.$array_data['texto'].'",'.$array_data['numerico'].',"'.$array_data['fecha'].'",NOW())');
+            $query = Db::getInstance()->execute('INSERT INTO '.$this->table.'(name,number,fecha,fecha_creacion) VALUES ("'.$array_data['texto'].'",'.$array_data['numerico'].',"'.$array_data['fecha'].'",NOW())');
             return $query;
         }
         else{
@@ -40,12 +41,12 @@ class Resources{
      * @return $query result of the query, or false if the var is empty
      */
     function delete(string $name){    
-       $check_name = Db::getInstance()->executeS('SELECT * FROM test.tableto WHERE name="'.$name.'"');
+       $check_name = Db::getInstance()->executeS('SELECT * FROM '.$this->table.' WHERE name="'.$name.'"');
         if(count($check_name)===0){
-            return false;
+            return $check_name;
         }
         else{
-            $query = Db::getInstance()->execute('DELETEFROM test.tableto WHERE name="'.$name.'"');
+            $query = Db::getInstance()->execute('DELETE FROM test.tableto WHERE name="'.$name.'"');
             return $query;
         }
     } 
