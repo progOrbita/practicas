cffa06dc2ae76a8157964ec44a82e6c86f3fb3e3
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-10 13:28:42 +0200

    api purpose and process description with validate and the default case

---
+++ formulario/api.php
@@ -0,0 +1,38 @@
+<?php
+    /**
+     * Api that validate/add and remove data from a formulary
+     * Pasos:
+     * 1. Cargar PS_Version
+     * 2. Cargar la clase resource
+     * 3. cargar $accion = tools::getvalue('accion','default)
+     * 4. hacer el switch (accion).
+     *  case validate:
+     *  case add:
+     *  case delete:
+     *  case default: (Que da error)
+     * 5. hacer el return en cada caso del valor recibido
+     * 6. terminar (porque siempre se va a llamar y va a tener un valor) con echo json_encode($resultado)
+     * Cosas a añadir posteriormente.
+     * El check de isset de los campos del formulario.
+     * el array de los datos
+     */
+    if(!defined('_PS_VERSION_')){
+		require_once '../../config/config.inc.php';
+		require_once '../../init.php';
+	}
+    include "Resources.php";
+    
+    $accion = (string) Tools::getValue('action','default');
+    switch($accion){
+        case "validate":
+                $validate = new Resources();
+                $array_datos = ["texto" => Tools::getValue('texto',""),"numerico" => Tools::getValue('numerico',0),"fecha" => Tools::getValue('fecha',0)];             
+                $datos = $validate->validacion($array_datos);
+                echo json_encode($datos);
+        break;
+        case 'default':
+            $resultado = "function dont found";
+        break;
+       // echo json_encode($resultado);
+    }  
+?>
