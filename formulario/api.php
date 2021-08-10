47d5f37a463902b47f64c4140b67372c874b83ec
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-10 17:05:31 +0200

    removed resultado initializing

--- formulario/api.php
+++ formulario/api.php
@@ -1,27 +1,22 @@
 <?php
     /**
      * Api that validate/add and remove data from a formulary
-     * Pasos:
-     * 1. Cargar PS_Version
-     * 2. Cargar la clase resource
-     * 3. cargar $accion = tools::getvalue('accion','default)
-     * 4. hacer el switch (accion).
-     *  case validate:
-     *  case add:
-     *  case delete:
-     *  case default: (Que da error)
-     * 5. hacer el return en cada caso del valor recibido
-     * 6. terminar (porque siempre se va a llamar y va a tener un valor) con echo json_encode($resultado)
-     * Cosas a añadir posteriormente.
-     * El check de isset de los campos del formulario.
-     * el array de los datos
+     * Pasos a seguir:
+     * 1. Load PS_Version
+     * 2. Load la clase resource
+     * 3. Usetools::getvalue('$POST/$GET','default)
+     * 4. switch for all the cases: validate, save and delete with a default in case of error
+     * all of them have the functionality from validate/save/delete.php files
+     * 
+     * Small additions: 
+     * a resultado var which always return a value so json_encode is used once
+     * creating a resources object/class once and not in each case
      */
     if(!defined('_PS_VERSION_')){
 		require_once '../../config/config.inc.php';
 		require_once '../../init.php';
 	}
     include "Resources.php";
-    $resultado = "";
     
     $accion = (string) Tools::getValue('action','default');
     $api_functions = new Resources();
