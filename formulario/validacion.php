ab6a13cd0022159c41e521131be3d136ed002b1e
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-10 11:41:27 +0200

    added prestashop files, needed to call the class

--- formulario/validacion.php
+++ formulario/validacion.php
@@ -2,8 +2,12 @@
 /*
 	Dado unos campos, los valida enviando la respuesta
 */
+	if(!defined('_PS_VERSION_')){
+		require_once '../../config/config.inc.php';
+		require_once '../../init.php';
+	}
 	include 'Resources.php';
-	if(isset($_POST['texto']) && isset($_POST['numerico']) && isset($_POST['fecha'])){
+	if(isset($_POST['texto'])){
 	$validate = new Resources();
 	$array_datos = ["texto" => $_POST['texto'],"numerico" => $_POST['numerico'],"fecha" => $_POST['fecha']];
 	$datos = $validate->validacion($array_datos);
