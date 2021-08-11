738a90dfd5f5b3928c52c85e7eb489cb75a72d19
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-11 12:25:59 +0200

    update from 1.00 to 1.01 ver

--- formulario/update.php
+++ formulario/update.php
@@ -6,8 +6,13 @@
         require_once '../../config/config.inc.php';
         require_once '../../init.php';
     }
-        $query = "ALTER TABLE "._DB_PREFIX_."formulario ADD COLUMN fecha_eliminacion DATETIME, ADD COLUMN eliminado BIT DEFAULT 0";
-        Db::getInstance()->execute($query);
-    
+    $query = "ALTER TABLE "._DB_PREFIX_."formulario ADD COLUMN fecha_eliminacion DATETIME, ADD COLUMN eliminado BIT DEFAULT 0";
+    $check = Db::getInstance()->execute($query);
+    if($check){
+        echo "table updated to version 1.01";
+    }
+    else{
+        echo "Update couldn't be done";
+    }
 ?>
 
