6e54db44b64e66306becf3055704637955528c7e
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-11 11:58:47 +0200

    table 1.0

--- formulario/install.php
+++ formulario/install.php
@@ -7,16 +7,14 @@
         require_once '../../config/config.inc.php';
         require_once '../../init.php';
     }
-    include "update.php";
     $query = "CREATE TABLE IF NOT EXISTS "._DB_PREFIX_."formulario(
         ID int AUTO_INCREMENT PRIMARY KEY,
         name varchar(255),
         number int,
         fecha DATE,
         fecha_creacion DATETIME,
-        fecha_mod DATETIME
+        fecha_mod DATETIME,
         )";
     Db::getInstance()->execute($query);
-    updateTable();
     echo "tabla generada<br/>";
 ?>
