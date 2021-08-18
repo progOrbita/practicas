1e7187ae682fd7713a96e0f1fd644ddcdb2a60ff
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-18 18:43:29 +0200

    exercise 3 area of a circle

---
+++ actividades/Javascript/ejer03js.js
@@ -0,0 +1,18 @@
+/*
+Crear un documento HTML (ejer02js.html) en el que se incluya un documento Javascript (ejer02.js) 
+externo que realice el siguiente proceso:
+1º) Pedir dos valores numéricos enteros por teclado (a y b). Los leeremos utilizando la función 
+prompt.
+2º) Mostrar en una ventana de alerta (alert):
+a. La suma de los dos números
+b. El producto de los dos números
+c. La división de a entre b
+d. El resto de dividir a entre b
+*/
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        let numA = window.prompt("Indica el radio:");
+        let area = Math.PI*Math.pow(numA,2);
+        window.alert("Area: "+area);
+    }
+  };
