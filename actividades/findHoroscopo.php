7d7484e13dcd784ff76f93f8bcfcb7a6b54aefc0
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-18 16:36:32 +0200

    from switch to an array

--- actividades/findHoroscopo.php
+++ actividades/findHoroscopo.php
@@ -9,44 +9,21 @@
                 echo "<h1>HORÓSCOPO CHINO</h1>
                         <p>año de Nacimiento: <b>".$fecha."</b><br/>
                         <p>Eres:</p>";
-                        switch($fecha%12){
-                            case 0: //1992 por ejemplo
-                                echo "<img src='mono.png'>";
-                                break;
-                            case 1:
-                                echo "<img src='gallo.png'>";
-                                break;
-                            case 2:
-                                echo "<img src='perro.png'>";
-                                break;
-                            case 3:
-                                echo "<img src='cerdo.png'>";
-                                break;
-                            case 4:
-                                echo "<img src='raton.png'>";
-                                break;
-                            case 5:
-                                echo "<img src='buey.png'>";
-                                break;   
-                            case 6:
-                                echo "<img src='tigre.png'>";
-                                break; 
-                            case 7:
-                                echo "<img src='conejo.png'>";
-                                break;
-                            case 8:
-                                echo "<img src='dragon.png'>";
-                                break;
-                            case 9:
-                                echo "<img src='serpiente.png'>";
-                                break;
-                            case 10:
-                                echo "<img src='caballo.png'>";
-                                break;
-                            case 11:
-                                echo "<img src='oveja.png'>";
-                               break;
-                        }
+                $horoscopos = [ 
+                    0 => "<img src='mono.png'>",
+                     1 => "<img src='gallo.png'>",
+                     2 => "<img src='perro.png'>",
+                     3 => "<img src='cerdo.png'>",
+                     4 => "<img src='raton.png'>",
+                     5 => "<img src='buey.png'>",
+                     6 => "<img src='tigre.png'>",
+                     7 => "<img src='conejo.png'>",
+                     8 => "<img src='dragon.png'>",
+                     9 => "<img src='serpiente.png'>",
+                     10 => "<img src='caballo.png'>",
+                     11 => "<img src='cabra.png'>"
+                ];
+                echo $horoscopos[$fecha%12];
                 echo "<br/><a href=horoscopo.php>Volver</a>";
             }
         ?>
