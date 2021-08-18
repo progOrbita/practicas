226f4f2b875967f64d1d422125dc694f9db86696
progOrbita <88653237+progOrbita@users.noreply.github.com>
2021-08-18 17:29:39 +0200

    factorial of a number sent

---
+++ actividades/factorial.php
@@ -0,0 +1,27 @@
+<?php
+    echo '<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Signo del zodiaco</title>
+        <meta charset="UTF-8"/>
+    <style type="text/css">
+        body{
+            margin-left: 45%;
+        }
+        input{
+            margin: 5px;
+        }
+        input[type="number"]{
+            width: 75px;
+        }
+    </style>
+    </head>
+    <body>
+        <h2>FACTORIAL:</h2>
+        <form method="GET" action="factorialNum.php">
+            <label>Numero:</label><input type="number" name="numero"><br/>
+            <input type="submit" value="enviar" name="submit" id="submit">
+        </form>
+    </body>
+    </html>';
+?>
---
+++ actividades/factorialNum.php
@@ -0,0 +1,32 @@
+<?php
+    echo '<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Signo del zodiaco</title>
+        <meta charset="UTF-8"/>
+    <style type="text/css">
+        body{
+            margin-left: 45%;
+        }
+        input{
+            margin: 5px;
+        }
+        input[type="number"]{
+            width: 75px;
+        }
+    </style>
+    </head>
+    <body>';
+        if(isset($_GET['numero'])){
+            $num = $_GET['numero'];
+            $total = 1;
+            for ($i=1; $i <= $num; $i++) { 
+                $total *= $i;
+            }
+            echo "El Factorial de <b>".$num."</b> es <b>".$total."</b>";
+        }
+    echo'
+    <br/><a href="factorial.php">volver</a>
+    </body>
+    </html>';
+?>
